ffe0b21996e48f84763ab4dd5cc25c160f0dfa17
Harsha Kolachina <sxk180148@utdallas.edu>
2020-07-13 20:43:34 -0500

    Fixed url issue for interParse

--- scrape.py
+++ scrape.py
@@ -78,9 +78,10 @@ for year in years:
                     index_split_accession = ''.join(index_split_modify[3].strip('.txt').split('-'))
                     for x in index_split_modify[:-1]:
                         index_split[4] = index_split[4] + '/' + x
-                    index_report_period_url = 'https://www.sec.gov/Archives/' + index_split_accession + '/' + index_split_modify[-1].strip('.txt') + '-index.htm'
+                    index_report_period_url = "https://www.sec.gov/Archives" + index_split[4] + '/' + index_split_accession + '/' + index_split_modify[-1].strip('.txt') + '-index.htm'
                     index_split[4] = index_split[4] + '/' + index_split_accession + '/' + index_split_modify[-1]
                     index_split[4] = "https://www.sec.gov/Archives" + index_split[4]
+                    print(index_report_period_url)
 
                     # Create the dictionary
                     filing_dict = dict(zip(headers, index_split))
