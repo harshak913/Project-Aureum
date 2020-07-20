086db0ce7cb35004b0231f63d63567710eb27d03
Harsha Kolachina <sxk180148@utdallas.edu>
2020-07-19 20:54:52 -0500

    Update scrape.py to track master IDX file data in the database master_idx table

--- scrape.py
+++ scrape.py
@@ -44,70 +44,79 @@ for year in years:
 
             # Grab the Master IDX file URL
             if 'master' in fileURL:
+                cursor.execute("SELECT status FROM database.master_idx WHERE master_file='%s'"%(file['name']))
+                status = cursor.fetchone()
+                if status == 'COMPLETED':
+                    continue
+                else:
+                    if len(cursor.fetchall()) == 0:
+                        cursor.execute("INSERT INTO database.master_idx (master_file, status) VALUES ('%s', '%s')"%(file['name'], 'PENDING'))
+                    elif status == 'ERROR':
+                        pass
+                    # Request that new content, this will NOT be a JSON STRUCTURE
+                    fileContent = requests.get(fileURL).content
 
-                # Request that new content, this will NOT be a JSON STRUCTURE
-                fileContent = requests.get(fileURL).content
+                    with open('master_file_text.txt', 'wb') as f:
+                        f.write(fileContent)
 
-                with open('master_file_text.txt', 'wb') as f:
-                    f.write(fileContent)
+                    # We now have a byte stream of data
+                    with open('master_file_text.txt','r') as f:
+                        byteData = f.readlines()
 
-                # We now have a byte stream of data
-                with open('master_file_text.txt','r') as f:
-                    byteData = f.readlines()
+                    os.remove('master_file_text.txt')
 
-                os.remove('master_file_text.txt')
+                    headers = byteData[5].strip().split('|')
+                    headers = [header.lower().replace(' ','') for header in headers]
+                    fileData = byteData[7:]
 
-                headers = byteData[5].strip().split('|')
-                headers = [header.lower().replace(' ','') for header in headers]
-                fileData = byteData[7:]
+                    # We need to split the data into sections so we can get each row value
+                    for index in fileData:
 
-                # We need to split the data into sections so we can get each row value
-                for index in fileData:
+                        # Clean it up.
+                        index_split = index.strip().split('|')
+                        if index_split[2] != '10-K' and index_split[2] != '10-Q':
+                            continue
+                        ciks = cursor.execute('''SELECT * FROM database.company WHERE cik=%s'''%(int(index_split[0])))
+                        if len(cursor.fetchall()) < 1:
+                            continue
 
-                    # Clean it up.
-                    index_split = index.strip().split('|')
-                    if index_split[2] != '10-K' and index_split[2] != '10-Q':
-                        continue
-                    ciks = cursor.execute('''SELECT * FROM database.company WHERE cik=%s'''%(int(index_split[0])))
-                    if len(cursor.fetchall()) < 1:
-                        continue
+                        # Create the URL
+                        index_split_modify = index_split[4].split('/')
+                        index_split[4] = ''
+                        index_split_accession = ''.join(index_split_modify[3].strip('.txt').split('-'))
+                        for x in index_split_modify[:-1]:
+                            index_split[4] = index_split[4] + '/' + x
+                        index_report_period_url = "https://www.sec.gov/Archives" + index_split[4] + '/' + index_split_accession + '/' + index_split_modify[-1].strip('.txt') + '-index.htm'
+                        index_split[4] = index_split[4] + '/' + index_split_accession + '/' + index_split_modify[-1]
+                        index_split[4] = "https://www.sec.gov/Archives" + index_split[4]
 
-                    # Create the URL
-                    index_split_modify = index_split[4].split('/')
-                    index_split[4] = ''
-                    index_split_accession = ''.join(index_split_modify[3].strip('.txt').split('-'))
-                    for x in index_split_modify[:-1]:
-                        index_split[4] = index_split[4] + '/' + x
-                    index_report_period_url = "https://www.sec.gov/Archives" + index_split[4] + '/' + index_split_accession + '/' + index_split_modify[-1].strip('.txt') + '-index.htm'
-                    index_split[4] = index_split[4] + '/' + index_split_accession + '/' + index_split_modify[-1]
-                    index_split[4] = "https://www.sec.gov/Archives" + index_split[4]
-
-                    # Create the dictionary
-                    filing_dict = dict(zip(headers, index_split))
-                    text_filing = filing_dict['filename']
-                    response = requests.get(text_filing)
-                    soup = BeautifulSoup(response.content, 'lxml')
-                    if len(soup.find_all('filename')) == 0:
-                        continue
-                    if '.htm' not in soup.find_all("filename")[0].get_text():
-                        continue
-                    accession_number = text_filing.split('/')[-1].strip('.txt') #Extract accession number (after last '/') & remove '.txt'
-                    accession_exist = cursor.execute("SELECT * FROM database.scrape WHERE accession_number = '%s'"%(accession_number))
-                    if len(cursor.fetchall()) > 0:
-                        print(f'Already inserted -- Link: {filing_dict["filename"]}, Date: {filing_dict["datefiled"]}')
-                        continue
-                    url_list = text_filing.split('/')
-                    url_xml = ''
-                    for part in url_list[:-1]:
-                        url_xml = url_xml + '' + part + '/'
-                    url_xml = url_xml + 'FilingSummary.xml'
-                    tree = ET.fromstring(requests.get(url_xml).text)
-                    if 'NoSuchKey' in tree[0].text and index_split[2] == '10-K':
-                        sql_statement = "INSERT INTO database.scrape (cik_id, filing_type, year, file_name, accession_number, inter_or_htm) VALUES(%s, '%s', %s, '%s', '%s', '%s');"%(int(filing_dict["cik"]), filing_dict["formtype"], filing_dict["datefiled"][0:4], filing_dict["filename"], accession_number, 'HTM')
-                        cursor.execute(sql_statement)
-                        print(sql_statement)
-                    elif 'NoSuchKey' not in tree[0].text and (index_split[2] == '10-K' or index_split[2] == '10-Q'):
-                        sql_statement = "INSERT INTO database.scrape (cik_id, filing_type, year, file_name, accession_number, inter_or_htm) VALUES(%s, '%s', %s, '%s', '%s', '%s');"%(int(filing_dict["cik"]), filing_dict["formtype"], filing_dict["datefiled"][0:4], filing_dict["filename"], accession_number, 'Inter')
-                        cursor.execute(sql_statement)
-                        print(sql_statement)
-                        interParse(index_report_period_url, accession_number, index_split[2])
+                        # Create the dictionary
+                        filing_dict = dict(zip(headers, index_split))
+                        text_filing = filing_dict['filename']
+                        response = requests.get(text_filing)
+                        soup = BeautifulSoup(response.content, 'lxml')
+                        if len(soup.find_all('filename')) == 0:
+                            continue
+                        if '.htm' not in soup.find_all("filename")[0].get_text():
+                            continue
+                        accession_number = text_filing.split('/')[-1].strip('.txt') #Extract accession number (after last '/') & remove '.txt'
+                        accession_exist = cursor.execute("SELECT * FROM database.scrape WHERE accession_number = '%s'"%(accession_number))
+                        if len(cursor.fetchall()) > 0:
+                            print(f'Already inserted -- Link: {filing_dict["filename"]}, Date: {filing_dict["datefiled"]}')
+                            continue
+                        url_list = text_filing.split('/')
+                        url_xml = ''
+                        for part in url_list[:-1]:
+                            url_xml = url_xml + '' + part + '/'
+                        url_xml = url_xml + 'FilingSummary.xml'
+                        tree = ET.fromstring(requests.get(url_xml).text)
+                        if 'NoSuchKey' in tree[0].text and index_split[2] == '10-K':
+                            sql_statement = "INSERT INTO database.scrape (cik_id, filing_type, year, file_name, accession_number, inter_or_htm) VALUES(%s, '%s', %s, '%s', '%s', '%s');"%(int(filing_dict["cik"]), filing_dict["formtype"], filing_dict["datefiled"][0:4], filing_dict["filename"], accession_number, 'HTM')
+                            cursor.execute(sql_statement)
+                            print(sql_statement)
+                        elif 'NoSuchKey' not in tree[0].text and (index_split[2] == '10-K' or index_split[2] == '10-Q'):
+                            sql_statement = "INSERT INTO database.scrape (cik_id, filing_type, year, file_name, accession_number, inter_or_htm) VALUES(%s, '%s', %s, '%s', '%s', '%s');"%(int(filing_dict["cik"]), filing_dict["formtype"], filing_dict["datefiled"][0:4], filing_dict["filename"], accession_number, 'Inter')
+                            cursor.execute(sql_statement)
+                            print(sql_statement)
+                            interParse(index_report_period_url, accession_number, index_split[2])
+                    cursor.execute("UPDATE database.master_idx SET status='%s' WHERE master_file='%s'"%('COMPLETED', file['name']))
