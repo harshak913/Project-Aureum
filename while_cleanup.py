b653a8387d637beb7857f83bb0f18debd3100eed
Harsha Kolachina <sxk180148@utdallas.edu>
2020-07-26 11:53:02 -0500

    deleted extra line

--- while_cleanup.py
+++ while_cleanup.py
@@ -5,17 +5,9 @@ while complete == False:
     for item in years:
         if item['status'] == 'pending':
             unfinished.append(item)
-<<<<<<< HEAD
-    for item in unfinished:
-        item.interParse
-    for item in unfinished:
-        if sta
-        
-=======
     if not unfinished:
         for item in unfinished:
             item.interParse
             #after parsing see if these things are completed if not then keep as pending, if it did complete parse change status to complete
     else:
         break
->>>>>>> 38823bcbcf83bf56461d8e1a0111e414092c0595
