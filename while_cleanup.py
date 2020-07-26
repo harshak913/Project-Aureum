de76f1ab9f7058007e089c48a0021bb370d91d5c
Andrew Zang <andrew.zang@utdallas.edu>
2020-07-25 22:34:59 -0500

    Create while_cleanup.py

---
+++ while_cleanup.py
@@ -0,0 +1,11 @@
+complete = False
+unfinished = []
+years = ['row1', 'row2', 'row3']
+while complete == False:
+    for item in years:
+        if item['status'] == 'pending':
+            unfinished.append(item)
+    for item in unfinished:
+        item.interParse
+    for item in unfinished:
+        if sta
