d3ee9b00658863e596cc0ee317e6591ad71f5ec9
Andrew Zang <andrew.zang@utdallas.edu>
2021-06-09 17:32:46 -0500

    Article Updates

--- Algo/ArtRss.py
+++ Algo/ArtRss.py
@@ -5,28 +5,26 @@ import re
 connection = psycopg2.connect(host="ec2-34-197-188-147.compute-1.amazonaws.com", dbname="d7p3fuehaleleo", user="snbetggfklcniv", password="7798f45239eda70f8278ce3c05dc632ad57b97957b601681a3c516f37153403a")
 connection.autocommit = True
 cursor = connection.cursor()
-'''
+
 #company specific news------------->
 scrape_query = "select * from company;"
 cursor.execute(scrape_query)
 entries = cursor.fetchall()
-breakOff = False
 
 for entry in entries:
     if entry[0] != 0:
         ticker = entry[1]
-        name = re.sub(r'\W+', '', entry[2])
+        #name = re.sub(r'\W+', '', entry[2])
         cik = entry[0]
         #if cik == 315189:
         #    breakOff = True
-        print(name)
-        newsQ = "https://news.google.com/rss/search?q="+name+"&hl=en-US&gl=US&ceid=US%3Aen"
-        newsQ = "https://news.google.com/rss/search?q="+ticker+" ticker"+"&hl=en-US&gl=US&ceid=US%3Aen"
+        #print(name)
+        #newsQ = "https://news.google.com/rss/search?q="+name+"ticker&hl=en-US&gl=US&ceid=US%3Aen"
+        newsQ = "https://news.google.com/rss/search?q="+ticker+"-ticker&hl=en-US&gl=US&ceid=US%3Aen"
         NewsFeed = feedparser.parse(newsQ) #company specific
         #print(NewsFeed)
         i=0
         for item in NewsFeed['entries']:
-            #if i < 50 and breakOff == True:
             if i < 50:
                 title =item['title'].replace("'", "‘")
                 link = item['link'].replace("'", "‘")
@@ -35,44 +33,47 @@ for entry in entries:
                 i+=1
                 sql_statement = "INSERT INTO news (cik, title, link, date_published, source) VALUES('%s', '%s', '%s', '%s', '%s')"%(cik, title, link, date_published, source)
                 print(sql_statement)
-                cursor.execute(sql_statement)
+                #cursor.execute(sql_statement)
 
     #print(i)
-'''
+allCats = [
 #Business News ------------------------>DONE
-#NewsFeed = feedparser.parse("https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen") #general business news
-#cik = 0
+{'NewsFeed': "https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen" #general business news
+,'cik': 0},
 
 #US News-------------------------->DONE
-#NewsFeed = feedparser.parse('https://news.google.com/rss/topics/CAAqIggKIhxDQkFTRHdvSkwyMHZNRGxqTjNjd0VnSmxiaWdBUAE?hl=en-US&gl=US&ceid=US%3Aen')
-#cik = 1
+{'NewsFeed': 'https://news.google.com/rss/topics/CAAqIggKIhxDQkFTRHdvSkwyMHZNRGxqTjNjd0VnSmxiaWdBUAE?hl=en-US&gl=US&ceid=US%3Aen'
+,'cik': 1},
 
 #World News----------------------->
-#NewsFeed = feedparser.parse('https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen')
-#cik = 2
+{'NewsFeed': 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen'
+,'cik': 2},
 
 #Technology News------------------>
-#NewsFeed = feedparser.parse('https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGRqTVhZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen')
-#cik = 3
+{'NewsFeed': 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGRqTVhZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen'
+,'cik': 3},
 
 #Entertainment News--------------->
-#NewsFeed = feedparser.parse('https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen')
-#cik = 4
+{'NewsFeed': 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen'
+,'cik': 4},
 
 #Science News------------------------->
-#NewsFeed = feedparser.parse('https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp0Y1RjU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen')
-#cik = 5
+{'NewsFeed': 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp0Y1RjU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen'
+,'cik': 5},
 
 #Health News---------------------->
-NewsFeed = feedparser.parse('https://news.google.com/rss/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNR3QwTlRFU0FtVnVLQUFQAQ?hl=en-US&gl=US&ceid=US%3Aen')
-cik = 6
+{'NewsFeed': 'https://news.google.com/rss/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNR3QwTlRFU0FtVnVLQUFQAQ?hl=en-US&gl=US&ceid=US%3Aen'
+,'cik': 6},
+]
 
-
-for item in NewsFeed['entries']:
-    title =item['title'].replace("'", "‘")
-    link = item['link']
-    date_published = item['published']
-    source = item['source']['title'].replace("'", "‘")
-    sql_statement = "INSERT INTO news (cik, title, link, date_published, source) VALUES('%s', '%s', '%s', '%s', '%s')"%(cik, title, link, date_published, source)
-    print(sql_statement)
-    cursor.execute(sql_statement)
+for cats in allCats:
+    NewsFeed = feedparser.parse(cats['NewsFeed'])
+    cik = cats['cik']
+    for item in NewsFeed['entries']:
+        title =item['title'].replace("'", "‘")
+        link = item['link']
+        date_published = item['published']
+        source = item['source']['title'].replace("'", "‘")
+        sql_statement = "INSERT INTO news (cik, title, link, date_published, source) VALUES('%s', '%s', '%s', '%s', '%s')"%(cik, title, link, date_published, source)
+        print(sql_statement)
+        #cursor.execute(sql_statement)
